b894da8321f9289becaf72be9ba0194468609bb0
wangzhaoxin <ittfxin@126.com>
2019-01-04 16:05:27 +0800

    添加部分方法

--- src/main/java/xin/framework/hybrid/utils/ADFilterTool.java
+++ src/main/java/xin/framework/hybrid/utils/ADFilterTool.java
@@ -17,6 +17,12 @@ public class ADFilterTool {
 
             js.append("var adDiv").append(i).append("= document.getElementById('").append(adDivs[i]).append("');if(adDiv").append(i).append(" != null)adDiv").append(i).append(".parentNode.removeChild(adDiv").append(i).append(");");
         }
+
+       /* js.append("var divs = document.querySelectorAll('[id^=s2]');").
+                append("if(divs!=null&&divs.length!=0) for(let index in divs) {  ")
+                .append(" divs[index].parentNode.removeChild(divs[index]);}");
+
+*/
         return js.toString();
     }
 }
